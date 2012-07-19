1b7a5b26fde287734ba4a921ddb5d5c3e1ff179d
richard@onlinegroups.net <richard@onlinegroups.net>
2012-07-19 23:41:48 +1200

    Add batching

--- gs/email/core.py
+++ gs/email/core.py
@@ -5,14 +5,24 @@ from config import create_emailUtilities
 import logging
 
 log = logging.getLogger('gs.email')
+max_batch = 50
 
-def send_email(sender, recipients, email, xverp=False):
+def send_email(sender, recipients, email):
     if not (isinstance(recipients, list) or isinstance(recipients, tuple)):
         recipients = [recipients]
 
     create_emailUtilities()
         
     mailer = getUtility(IMailDelivery, 'gs.maildelivery')
-    mailer.send(sender, recipients, email)
-    log.info("Sent email of length: %s to %s from %s" %
-                                      (len(email), recipients, sender))
+
+    while recipients:
+        if (max_batch == 0) or (max_batch > len(recipients)):
+            batch = len(recipients)
+        else:
+            batch = batchsize
+        
+        mailer.send(sender, recipients[0:batch], email)
+        log.info("Sent email of length: %s to %s (batchsize: %s) from %s" %
+                           (len(email), recipients[0:batch], batch, sender))
+        
+        recipients = recipients[batch:]
