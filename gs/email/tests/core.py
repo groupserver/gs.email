28c24b0259bb2858c515ca3631b0e6b13202c365
Michael JasonSmith <mpj17@onlinegroups.net>
2015-03-17 19:49:02 -0700

    Tidying the tests. Batching actually goes

--- gs/email/tests/core.py
+++ gs/email/tests/core.py
@@ -13,21 +13,23 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals
-from mock import patch
+from mock import (call, patch)
 from unittest import TestCase
 from gs.email.core import send_email
 import gs.email.core
 
 
 class TestCore(TestCase):
+    sender = 'durk@example.com'
     addrs = ['person@people.example.com', 'member@members.example.com',
              'individual@people.example.com', 'admin@members.example.com', ]
+    email = 'This is an email'
 
     @patch('gs.email.core.getUtility')
     @patch('gs.email.core.create_emailUtilities')
     def test_no_dest(self, ceuMock, getUtilityMock):
         mailerMock = getUtilityMock()
-        send_email('durk@example.com', [], 'This is an email')
+        send_email(self.sender, [], self.email)
         self.assertEqual(0, mailerMock.send.call_count)
 
     @patch('gs.email.core.getUtility')
@@ -35,8 +37,7 @@ class TestCore(TestCase):
     def test_str_dest(self, ceuMock, getUtilityMock):
         'Test when the destination is a string'
         mailerMock = getUtilityMock()
-        send_email('durk@example.com', 'dinsdale@example.com',
-                   'This is an email')
+        send_email(self.sender, self.addrs[0], self.email)
         self.assertEqual(1, mailerMock.send.call_count)
 
     @patch('gs.email.core.getUtility')
@@ -44,8 +45,9 @@ class TestCore(TestCase):
     def test_list_dest(self, ceuMock, getUtilityMock):
         'Test when the destination is a list'
         mailerMock = getUtilityMock()
-        send_email('durk@example.com', self.addrs, 'This is an email')
-        self.assertEqual(1, mailerMock.send.call_count)
+        send_email(self.sender, self.addrs, self.email)
+        mailerMock.send.assert_called_once_with(self.sender, self.addrs,
+                                                self.email)
 
     @patch('gs.email.core.getUtility')
     @patch('gs.email.core.create_emailUtilities')
@@ -53,6 +55,8 @@ class TestCore(TestCase):
         'Do we batch the requests'
         gs.email.core.max_batch = 2
         mailerMock = getUtilityMock()
-        send_email('durk@example.com', self.addrs, 'This is an email')
-        self.assertEqual(2, mailerMock.send.call_count)
+        send_email(self.sender, self.addrs, self.email)
+        calls = [call(self.sender, self.addrs[:2], self.email),
+                 call(self.sender, self.addrs[2:], self.email), ]
+        mailerMock.send.has_calls(calls)
         gs.email.core.max_batch = 50
